0248d49946c72b8802c0457f103805ebe617dea8
bli 89 <benjaminli000@gmail.com>
2019-07-01 23:31:36 +0930

    Initial Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Homework #1 of Python is Easy
+# Musician
+Artist = "Van Halen"
+# Title of the Song
+Song = "Jump"
+# Genre of Music
+Genre = "Rock"
+# Duration of Song
+TimeInSeconds = 243
+# Date Song was Released
+YearReleased = "1984"
+# Record Company
+Label = "Warner Bros"
+# Initial Music Format
+Format = "CD Single"
+"""
+Rating rating out of 
+5
+"""
+Rating = 4.5
+
+print(Artist)
+print(Song)
+print(Genre)
+print(TimeInSeconds)
+print(YearReleased)
+print(Label)
+print(Format)
+print(Rating)
